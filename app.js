166b5e8965e48de9d0087af502b2ce0540333515
Shayan-Irfan <ishayan767@gmail.com>
2020-12-24 15:03:44 +0500

    calculator

---
+++ app.js
@@ -0,0 +1,19 @@
+// var a = "A";
+// a.charCodeAt()
+// console.log(a)
+
+
+function getNumber(num) {
+     var result = document.getElementById("result");
+     result.value += num;
+}
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = ''
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
